7c7f43638c1b70aeb7cde02c923886088df41d94
Suman Singh <suman.singh@intl.verizon.com>
2018-09-11 16:35:42 +0530

    added routes

--- src/client/App.js
+++ src/client/App.js
@@ -1,30 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
 import Main from './dashboard/Main';
 
 import './app.css';
 
+const PlayerSelection = () => <div> player selection here</div>;
+
+const PreviewTeam = () => <div> Preview Team here</div>;
+
 const App = () => (
   <Router>
     <Paper elevation={1}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
-
-      <hr />
-
       <Route exact path="/" component={Main} />
-      {/* <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} /> */}
+      <Route exact path="/player" component={PlayerSelection} />
+      <Route exact path="/team" component={PreviewTeam} />
     </Paper>
   </Router>
 );
